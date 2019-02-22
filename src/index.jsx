5f474bf7956c99ddf73319bd595782f6915d28af
qi.yang <qi.yang@transwarp.io>
2019-02-22 10:57:36 +0800

    修改样式

--- src/index.jsx
+++ src/index.jsx
@@ -4,19 +4,21 @@ import { AppContainer } from 'react-hot-loader'; // 保存state
 import 'babel-polyfill';
 import '../node_modules/antd/dist/antd.min.css';
 import './index.less';
-import MarkDown from './MarkDown';
+import MarkDown, {MarkDownMode} from './MarkDown';
 
 const app = document.getElementById('app');
 
 function renderApp(Component)  {
     ReactDOM.render(
         <AppContainer>
-            <Component />
+            {
+                Component
+            }
         </AppContainer>, app
     )
 }
 
-renderApp(MarkDown)
+renderApp(<MarkDown mode={MarkDownMode.EDIT} />)
 
 if (module.hot) {                                // 热更新时，保存state
     module.hot.accept('./MarkDown', () => {
